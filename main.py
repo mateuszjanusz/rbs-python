240eae721ad199bc7cea10a9fd3230d48db88404
Mateusz Janusz <mateuszjanusz95@gmail.com>
2017-12-03 16:11:09 +0000

    backward chaining algo fixed

--- main.py
+++ main.py
@@ -1,61 +1,44 @@
 def breakDownRules(my_rules):
 	rules = {}
+	print("Rules:")
 	for rule in my_rules: 
 		if_ = rule.split('THEN')[0]
 		then = rule.split('THEN')[1]
-		# print("if", if_, "then", then)
+		print("  if", if_, "then", then)
 		rules[then] = if_
 
 	return rules
 
 
 def backwardChaining(goal, rules, working_memory):
-	# goals = [goal]
-
-	print("rules: ", rules)
-	print("wm: ", working_memory)
-	print("goal: ", goal)
-
-	print("Start")
+	print("Backward Chaining for goal:", goal)
 	for fact in working_memory:
 		if(goal != fact):
-			print("match NOT found, goal:",goal, "fact:", fact)
+			# print("match NOT found, goal:",goal, "fact:", fact)
 			for consequent in rules:
 				if(consequent == goal):
-					print("consequent:", consequent, "matches goal:", goal)
 					antecedent = rules[consequent]
-					print("antecedent: ", antecedent)
-					backwardChaining(antecedent, rules, working_memory)
-
-
+					print("! consequent matches goal:", goal, "in rule: ' IF", antecedent, "THEN", consequent,"'")
+					print("antecedent (is new subgoal):", antecedent)
+					return backwardChaining(antecedent, rules, working_memory)
 		else:
-			print("match found, goal:", goal,"fact:", fact)
-			print("RULE FIRES")
-	# for goal in goals:
-	# 	print("GOAL: ", goal)
-	# for rule in rules: 
-	# 	consequent = rules[rule]
-
-	# 	for fact in working_memory:
-	# 		if(fact == consequent):
-	# 			print("FACT: ", fact, " >  ", consequent)
-
-	# 	if(goal == consequent):
-	# 		print(goal, " > new goal: ", rule)
-	# 		goals.append(rule)
-	# 		print(goals)
+			print("Goal proven!  goal:", goal,"fact:", fact)
+			return True
+	return False
 
 
 
 
 
-
-
-# my_rules = ['fh&ac=bg', 'ns=em', 'rt=pq', 'dj&em&ki=ac', 'pq=ns', 'uv=ki']
+# my_rules = ['fh&acTHENbg', 'nsTHENem', 'rtTHENpq', 'dj&em&kiTHENac', 'pqTHENns', 'uvTHENki']
 simple_rules = ['acTHENbg', 'nsTHENem', 'rtTHENpq', 'kiTHENac', 'pqTHENns', 'uvTHENki']
 my_working_memory = ['fh', 'dj', 'uv', 'rt']
 
 rules = breakDownRules(simple_rules)
 
-backwardChaining('em', rules, my_working_memory)
-
+print("working memory: ", my_working_memory)
+print()
+if(backwardChaining('bg', rules, my_working_memory)):
+	print("\n <<<< Hypothesis is true >>>>")
+else:
+	print("\n <<<< Hypothesis is false >>>>")
